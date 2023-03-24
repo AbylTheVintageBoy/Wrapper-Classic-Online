2f3eb1c7fd2020c0f77da280cf480a5f3f5979e4
Junior-Is-Great67 <65474160+Junior-Is-Great67@users.noreply.github.com>
2023-03-23 23:50:40 -0400

    Files for japanese

---
+++ Wrapper Classic - The Golden Days of Goanimate_files/i.js
@@ -0,0 +1,34 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var KMCID='JwjqXkvAzR4zl70LoTk5eV6r6QQ';if(typeof(_kmil) == 'function')_kmil();
+
+}
+/*
+     FILE ARCHIVED ON 07:31:12 Jun 02, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:48:29 Mar 24, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.067
+  exclusion.robots.policy: 0.059
+  cdx.remote: 0.126
+  esindex: 0.008
+  LoadShardBlock: 243.319 (6)
+  PetaboxLoader3.datanode: 185.911 (7)
+  PetaboxLoader3.resolve: 221.645 (3)
+  load_resource: 191.613
+*/
